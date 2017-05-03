34c7c4f5e24ecedfc209fea6091d9905bbcabd40
Trevor Bramble <tbramble@chef.io>
2017-05-02 17:19:09 -0700

    Initial gem generation, definition

---
+++ lib/marketo_chef.rb
@@ -0,0 +1,8 @@
+require "marketo_chef/version"
+
+require 'faraday'
+require 'faraday_middleware'
+
+module MarketoChef
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'marketo_chef'
+
+require 'minitest/autorun'
