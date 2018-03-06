c60f7e08e030f8d5d41aa3ca712ca07865cf7df3
Trevor Bramble <tbramble@chef.io>
2018-03-05 17:45:11 -0800

    Release 1.1.0 under Apache 2.0 license

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.1.0] - 2018.03.06
+### Added
+- Gem is now licensed Apache 2.0
+
+### Changed
+- Allow pushing the gem to RubyGems.org
+
 ## [1.0.2] - 2017.07.06
 ### Changed
 - Marketo error codes are declared as Strings
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
+# Copyright 2018 Chef Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # remove these when we can configure client directly
 ENV['MARKETO_HOST']          = '123-ABC-456.mktorest.com'
 ENV['MARKETO_CLIENT_ID']     = 'c4206ec5-be87-465c-a0cc-99486a4c4e1b'
 ENV['MARKETO_CLIENT_SECRET'] = 'Z5bzTySMrFdgcFZkSNvBywMuUen9ah7Q'
 ENV['MARKETO_CAMPAIGN_ID']   = '1234'
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'marketo_chef'
 
 require 'minitest/autorun'
